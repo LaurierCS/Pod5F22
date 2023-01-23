c936554f080a3cbc76fd90ce639d0292d567107f
danij12 <235danieljames@gmail.com>
2023-01-23 15:37:53 -0500

    Frontend boiler plate code

--- umotive/src/LoginPage.js
+++ umotive/src/LoginPage.js
@@ -3,14 +3,32 @@ import { Link, Redirect } from 'react-router-dom';
 const LoginPage = () => {
     const [email,setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [error, setError] = useState('');
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+
+        if(email==='admin@example.com' && password ==='admin'){
+            setIsLoggedIn(true)
+        }
+        else{
+            setError('Invalid email or password');
+        }
+    };
+
+    if (isLoggedIn){
+       return <Redirect to="/" />
+    }
 
     return (  
         
-        <div className="login">
-            <form >
+        <div className="Login">
+            <form onSubmit={handleSubmit}>
                 <label>E-mail: <input  Required type="email" value={email} onChange={(e) => setEmail(e.target.value)}> </input> </label>
                 <label type="password">Password: <input Required type="password" value={password} onChange={(e) => setPassword(e.target.value)}></input> </label>
                 <button type="submit">Enter</button>
+                {error && <div>{error}</div>}
             </form>
             <Link to="/signup">Don't have an account? Sign up</Link>
         </div>
